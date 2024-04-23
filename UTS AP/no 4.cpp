005c5be398faccef9b969dba728fbcd0955baaef
divanimodena <147525871+divanimodena@users.noreply.github.com>
2024-04-23 09:37:35 +0700

    Update no 4.cpp

--- UTS AP/no 4.cpp
+++ UTS AP/no 4.cpp
@@ -3,7 +3,7 @@
 int main() {
     int rows = 7; // Ubah nilai rows sesuai dengan tinggi belah ketupat yang diinginkan
     
-    // Bagian atas belah ketupat
+    
     for (int i = 1; i <= rows; ++i) {
         for (int j = 1; j <= rows - i; ++j) {
             std::cout << "  ";
@@ -14,7 +14,7 @@ int main() {
         std::cout << std::endl;
     }
     
-    // Bagian bawah belah ketupat
+    
     for (int i = rows - 1; i >= 1; --i) {
         for (int j = 1; j <= rows - i; ++j) {
             std::cout << "  ";
